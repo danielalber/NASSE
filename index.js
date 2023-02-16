86b9b1e054b3f1699d3da6bba1473d6428a6df85
daniel.albergucci <daniel.albergucci@epitech.eu>
2023-02-16 14:11:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,20 @@ app.use(
     swaggerUi.setup(specs)
 );
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
+    res.setHeader('Cross-origin-Opener-Policy', 'same-origin');
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200)
+    } else {
+        next()
+    }
+});
+
 app.use(helmet());
 
 app.use(bodyParser.json());
