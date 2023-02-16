4f18bfad9e86559b43aac1060c3a32d7e555304a
daniel.albergucci <daniel.albergucci@epitech.eu>
2023-02-16 12:26:56 +0100

    add swagger doc authentification

--- index.js
+++ index.js
@@ -13,6 +13,38 @@ const AppConfig = config.App;
 const MaillerConfig = config.Mailler;
 const finalConfig = _.merge(MongoDBConfig, MaillerConfig, AppConfig);
 
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Nasse Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple RESTFULL API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "ALBERGUCCI Daniel",
+                url: "https://youtube.com",
+                email: "daniel.albergucci@epitech.Eu",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:2546",
+            },
+        ],
+    },
+    apis: ["./src/routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
 global.gConfig = finalConfig;
 
 const app = express();
@@ -24,6 +56,12 @@ app.use(helmet());
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 
 const db = connectiondb.connectToDb();
 
